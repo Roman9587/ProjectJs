dc41052aff60c5c0c140c7840a963e7f3ffe7fa9
Roman9587 <rkim2972@gmail.com>
2020-07-05 10:28:26 +0600

    rework

--- scripthw.js
+++ scripthw.js
@@ -1,17 +1,38 @@
-let money, time, answerFirst,answerSecond;
 
-money = +prompt("ваш бюджет на месяц?");
+let money = prompt("ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-time = prompt ("Введите дату в формате YYYY-MM-DD");
-
-answerFirst = prompt("Введите обязательную статью расходов в этом месяце")
-
-answerSecond = prompt("Во сколько обойдется?")
-let budget = money, timeData = time  ;
+//    if (typeof(money === 'number') && typeof(money) != null && money != "" && typeof(time === 'number') && typeof(time) != null && time != "" ) {
+       
+//    }
+    
 let appData = {
-    budget : budget,
-    timeData : timeData,
-    arr:[],
+    budget : money,
+    timeData : time,
+    expenses : {},
+    income : [],
+    optionalExpenses : {},
     savings : false
 }
-alert("ваш бюджет на 1 день: " + money/30   );
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = +prompt("Во сколько обойдется?", "");
+    if (typeof (a) === 'string' && typeof (a) != null && a != "" && typeof (b) === 'number' && typeof (b) != null && b != "") {
+        appData.expenses[a] = b;
+    } else {
+        if(i > 0) {
+            i--;
+            console.log("Отнялось " + i);
+        }else{
+            i = -1;
+            console.log("Обнление " + i);
+        }
+    }
+}
+
+
+
+
+
+alert("ваш бюджет на 1 день: " + (appData.budget / 30).toFixed());
